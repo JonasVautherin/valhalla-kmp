35237c92576b9a6a056016d7bf1e56f5c01a690e
Jonas Vautherin <dev@jonas.vautherin.ch>
2026-04-30 00:59:06 +0200

    Patch valhalla to install static libs properly

--- src/main/cmake/FindValhalla.cmake
+++ src/main/cmake/FindValhalla.cmake
@@ -16,6 +16,17 @@ find_library(Valhalla_LIBRARY
     PATH_SUFFIXES lib
 )
 
+find_library(Valhalla_TYR_LIBRARY     NAMES valhalla-tyr     PATH_SUFFIXES lib)
+find_library(Valhalla_BALDR_LIBRARY   NAMES valhalla-baldr   PATH_SUFFIXES lib)
+find_library(Valhalla_LOKI_LIBRARY    NAMES valhalla-loki    PATH_SUFFIXES lib)
+find_library(Valhalla_THOR_LIBRARY    NAMES valhalla-thor    PATH_SUFFIXES lib)
+find_library(Valhalla_ODIN_LIBRARY    NAMES valhalla-odin    PATH_SUFFIXES lib)
+find_library(Valhalla_SIF_LIBRARY     NAMES valhalla-sif     PATH_SUFFIXES lib)
+find_library(Valhalla_MEILI_LIBRARY   NAMES valhalla-meili   PATH_SUFFIXES lib)
+find_library(Valhalla_SKADI_LIBRARY   NAMES valhalla-skadi   PATH_SUFFIXES lib)
+find_library(Valhalla_MIDGARD_LIBRARY NAMES valhalla-midgard PATH_SUFFIXES lib)
+find_library(Valhalla_PROTO_LIBRARY   NAMES valhalla-proto   PATH_SUFFIXES lib)
+
 # Extract version from valhalla/config.h
 if(Valhalla_INCLUDE_DIR AND EXISTS "${Valhalla_INCLUDE_DIR}/valhalla/config.h")
     file(STRINGS "${Valhalla_INCLUDE_DIR}/valhalla/config.h" _valhalla_version_line
@@ -30,18 +41,28 @@ find_package_handle_standard_args(Valhalla
 )
 
 if(Valhalla_FOUND AND NOT TARGET valhalla::valhalla)
-    # Pull in actual dependencies via find_package (they all have cmake modules)
     find_package(Protobuf REQUIRED)
     find_package(ZLIB REQUIRED)
     find_package(Boost REQUIRED)
 
+    # Collect whichever component libs were found
+    set(Valhalla_COMPONENT_LIBS "")
+    foreach(comp TYR BALDR LOKI THOR ODIN SIF MEILI SKADI MIDGARD PROTO)
+        if(Valhalla_${comp}_LIBRARY)
+            list(APPEND Valhalla_COMPONENT_LIBS "${Valhalla_${comp}_LIBRARY}")
+        endif()
+    endforeach()
+
     add_library(valhalla::valhalla STATIC IMPORTED)
     set_target_properties(valhalla::valhalla PROPERTIES
         IMPORTED_LOCATION "${Valhalla_LIBRARY}"
-        INTERFACE_INCLUDE_DIRECTORIES "${Valhalla_INCLUDE_DIR};${Valhalla_INCLUDE_DIR}/valhalla/third_party"
         INTERFACE_INCLUDE_DIRECTORIES "${Valhalla_INCLUDE_DIR};${Valhalla_INCLUDE_DIR}/valhalla;${Valhalla_INCLUDE_DIR}/valhalla/third_party"
-        INTERFACE_LINK_LIBRARIES "protobuf::libprotobuf-lite;ZLIB::ZLIB;Boost::boost"
+        INTERFACE_LINK_LIBRARIES "${Valhalla_COMPONENT_LIBS};protobuf::libprotobuf;ZLIB::ZLIB;Boost::boost"
     )
 endif()
 
-mark_as_advanced(Valhalla_INCLUDE_DIR Valhalla_LIBRARY)
+mark_as_advanced(Valhalla_INCLUDE_DIR Valhalla_LIBRARY
+    Valhalla_TYR_LIBRARY Valhalla_BALDR_LIBRARY Valhalla_LOKI_LIBRARY
+    Valhalla_THOR_LIBRARY Valhalla_ODIN_LIBRARY Valhalla_SIF_LIBRARY
+    Valhalla_MEILI_LIBRARY Valhalla_SKADI_LIBRARY Valhalla_MIDGARD_LIBRARY
+    Valhalla_PROTO_LIBRARY)
